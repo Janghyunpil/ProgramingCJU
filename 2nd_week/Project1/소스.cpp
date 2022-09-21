5ac4923f162b7c6255046eee6aec412ed9acd8d9
김윤수 <112995075+crissPbacon@users.noreply.github.com>
2022-09-21 15:11:17 +0900

    2nd_week
    2주차 project

---
+++ 2nd_week/Project1/소스.cpp
@@ -0,0 +1,7 @@
+int main(void)                         //50p 3번
+{
+	printf("학번:%d\n", 2019010712);
+	printf("이름:%s\n", "장현필");
+	printf("학점:%c\n", 'A');
+	return 0;
+}
---
+++ 2nd_week/Project2/소스.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)               //82p  3¹ø
+{
+	char ch;
+	printf("input charater:\n");
+	scanf_s("%c", &ch);
+	printf("your inpur:%c\n", ch);
+	printf("your input to ascii code:%d\n", ch);
+	return 0;
+}
