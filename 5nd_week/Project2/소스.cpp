9ace1e24984cc507db296ced6214d6b93b56e2f6
Janghyunpil <wkdgusvlf308@gmail.com>
2022-10-05 20:38:03 +0900

    5nd_week
    project2 수정

--- 5nd_week/Project2/소스.cpp
+++ 5nd_week/Project2/소스.cpp
@@ -1,33 +1,34 @@
 #include <stdio.h>
 #define MIN 2
+#define CMAX 5
 int main()
 {
 	int num;
 	printf("숫자를 입력해주세요\n");
 	scanf_s("%d", &num);
-	int divsize=1;
+	int divsize=0;
 	int counter=1;
 	for (int i = 1; i <= num; i++)
 	{
-		for (int j = 1; j < i; j++)
+		for (int j = 1; j <= i; j++)
 		{
 			if (i % j == 0)
 			{
 				divsize = divsize + 1;
 			}
 		}
-	if (divsize == 2)
+	if (divsize == MIN)
 		{
 			printf("%d	", i);
 			counter = counter + 1;
 		}
 	
-	if (counter == 5)
+	if (counter == CMAX)
 		{
 			counter = 1;
 			printf("\n");
 		}	
-	divsize = 1;
+	divsize = 0;
 	}
 	return 0;
 }
