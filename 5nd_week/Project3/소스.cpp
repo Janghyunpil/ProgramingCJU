80c8cc729e245ca1cefe4636037fe7bef42ddf0a
janghyunpil <wkdgusvlf308@gmail.com>
2022-10-05 17:31:18 +0900

    5nd_week
    Project 3

---
+++ 5nd_week/Project3/소스.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int SUM(int x, int y);
+int SUB(int x, int y);
+int MUL(int x, int y);
+int DIV(int x, int y);
+int main()
+{
+	int A = 0, B = 0, E = 0;
+	char Ch;
+	printf("수식을 입력해주세요 ");
+	scanf_s("%d %c %d", &A, &Ch, 1, &B);
+	if (Ch == '+')
+	{
+		E = SUM(A,B);
+	}
+	else if (Ch == '-')
+	{
+		E = SUB(A, B);
+	}
+	else if (Ch == '*')
+	{
+		E = MUL(A, B);
+	}
+	else if (Ch == '/')
+	{
+		E = DIV(A, B);
+	}
+	printf("결과값 : %d", E);
+	return 0;
+}
+int SUM(int x, int y) 
+{
+	int Res;
+	Res = x + y;
+	return Res;
+}
+int SUB(int x, int y)
+{
+	int Res;
+	Res = x - y;
+	return Res;
+}
+int MUL(int x, int y)
+{
+	int Res;
+	Res = x * y;
+	return Res;
+}
+int DIV(int x, int y)
+{
+	int Res;
+	Res = x / y;
+	return Res;
+}
