12972a928246778e91f471b07724600ba3d87cc9
Janghyunpil <wkdgusvlf308@gmail.com>
2022-10-22 00:23:30 +0900

    6th_week
    project 1 수정

--- 6th_week/Project1/소스.cpp
+++ 6th_week/Project1/소스.cpp
@@ -1,25 +1,39 @@
 #include <stdio.h>
+
 #define DIGITs 4
+
+int COUNT(int x,int y);
+
+int COUNT(int x, int y) 
+{
+	int count = 0;
+	if (x == y) count = 1;
+	return count;
+}
+
 int main()
 {
 	int i, j;
 	int count = 0;
 	int PW[DIGITs];
 	int nPW[DIGITs];
+
 	printf("Please register your password.\n");
+
 	for (i = 0; i < DIGITs; i++)
 	{
 		scanf_s("%d", &PW[i]);
 	}
+
 	while (count < DIGITs)
 	{
 		printf("Please enter your password.\n");
 		for (j = 0; j < DIGITs; j++)
 		{
 			scanf_s("%d", &nPW[j]);
-			if (PW[j] == nPW[j]) count=count+1;
+			count += COUNT(PW[j], nPW[j]);
 		}
-		if (count == 4)
+		if (count == DIGITs)
 		{
 			printf("normal termination\n");
 		}
@@ -32,4 +46,5 @@ int main()
 	}
 	return 0;
 }
+
 
