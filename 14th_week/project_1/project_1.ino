79c3e58acba912c4a3fb23d77f74260f51c61962
sovoscom <113013137+Janghyunpil@users.noreply.github.com>
2022-12-07 15:36:42 +0900

    Update project_1.ino

--- 14th_week/project_1/project_1.ino
+++ 14th_week/project_1/project_1.ino
@@ -22,7 +22,7 @@ void GenLEDOutPut(int x,int y)
 
 void setup()
 {
-  myservo.attach(8);
+  myservo.attach(8); //서보모터를 8로 초기화
   pinMode(nRedPin, OUTPUT);
   pinMode(nBluePin, OUTPUT);
   pinMode(nPushSwPin, INPUT);
@@ -31,19 +31,20 @@ void setup()
 
 void loop()
 {
- if(digitalRead(nPushSwPin) == HIGH)
+ if(digitalRead(nPushSwPin) == HIGH) //스위치 on off 여부 확인
  {
    Serial.print("Switch:ON\n");
    
    int readValue = analogRead(A0);
-   Serial.println(readValue);
-   int a = map(readValue,MINANALOG,MAXANALOG,MINDEGREE,MAXDEGREE);
-   for(int i = 0;i<a;i++)
+   Serial.println(readValue);         //가변저항 값 읽어오기
+   int a = map(readValue,MINANALOG,MAXANALOG,MINDEGREE,MAXDEGREE);   //가변저항값의 범위를 원하는 각도값의 범위로 변경
+   
+   for(int i = 0;i<a;i++)       //입력받은 각도로 서보모터를 회전
    {
      myservo.write(i);
    }
    
-   if(a<=90)
+   if(a<=90)      //서보모터가 90도 이하일 경우 빨간 불이 아닐경우 파란불이 깜박거리게 설정했습니다.
    {
      GenLEDOutPut(nRedPin,HIGH);
      GenLEDOutPut(nRedPin,LOW);
@@ -54,7 +55,7 @@ void loop()
      GenLEDOutPut(nBluePin,LOW);
    }
  }
-  else
+  else          // 스위치가 꺼져있는경우 모든 LED 끄기
   {
     Serial.print("Switch:OFF\n");
     GenLEDOutPut(nRedPin,LOW);
