bd110baed43da0402f94a0812495503002d1dba4
sovoscom <113013137+Janghyunpil@users.noreply.github.com>
2022-12-07 16:49:21 +0900

    Update project_1.ino

--- 14th_week/project_1/project_1.ino
+++ 14th_week/project_1/project_1.ino
@@ -13,7 +13,7 @@
 
 Servo myservo;
 
-int P[]={nRedpin,nBluepin,nPushSwPin,nDelay};
+int P[]={nRedpin,nBluepin,nPushSwPin,nDelay}; //배열,포인터 생성
 int *p=P;
 
 void GenLEDOutPut(int x,int y,int z)
@@ -24,7 +24,7 @@ void GenLEDOutPut(int x,int y,int z)
 
 void setup()
 {
-  myservo.attach(8);
+  myservo.attach(8);  //서보모터를 8번으로 초기화
   pinMode(*(p+0), OUTPUT);
   pinMode(*(p+1), OUTPUT);
   pinMode(*(p+3), INPUT);
@@ -35,17 +35,17 @@ void loop()
 {
  if(digitalRead(*(p+2)) == HIGH)
  {
-   Serial.print("Switch:ON\n");
+   Serial.print("Switch:ON\n"); 
    
-   int readValue = analogRead(A0);
-   Serial.println(readValue);
-   int a = map(readValue,MINANALOG,MAXANALOG,MINDEGREE,MAXDEGREE);
-   for(int i = 0;i<a;i++)
+   int readValue = analogRead(A0); //  가변저항 값 읽어오기
+   Serial.println(readValue); 
+   int a = map(readValue,MINANALOG,MAXANALOG,MINDEGREE,MAXDEGREE);  //가변저항 값을 원하는 범위 내의 값으로 재지정
+   for(int i = 0;i<a;i++)    //서보모터 각도 조절
    {
-     myservo.write(i);
+     myservo.write(i);   
    }
    
-   if(a<=90)
+   if(a<=90)    //서보모터 각도가 90도 이하일 때 빨간 불이 들어오고 아니면 파란불이 들어오도록 설정
    {
      GenLEDOutPut(*(p+0),HIGH,*(p+3));
      GenLEDOutPut(*(p+0),LOW,*(p+3));
@@ -56,7 +56,7 @@ void loop()
      GenLEDOutPut(*(P+1),LOW,*(p+3));
    }
  }
-  else
+  else          // 스위치가 꺼져있다면 모든 LED off
   {
     Serial.print("Switch:OFF\n");
     GenLEDOutPut(*(p+0),LOW,*(p+3));
